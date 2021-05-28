4af7654abb777deffdbea8607aad3a23a0a549b2
Alexey Kuldoshin <kuldoshin.alexey@gmail.com>
2021-05-28 06:09:19 +0300

    commin

--- common/httpclientpool.go
+++ common/httpclientpool.go
@@ -1,1 +1,28 @@
 package common
+
+import (
+	"net/http"
+	"sync"
+)
+
+type HttpClientPool struct {
+	pool sync.Pool
+}
+
+func NewHttpClientPool() HttpClientPool {
+	return HttpClientPool{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return &http.Client{}
+			},
+		},
+	}
+}
+
+func (httpClientPool *HttpClientPool) Get() *http.Client {
+	return httpClientPool.pool.Get().(*http.Client)
+}
+
+func (httpClientPool *HttpClientPool) Put(client *http.Client) {
+	httpClientPool.pool.Put(client)
+}
