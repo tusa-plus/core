a1134e75224dff7b9f9798cbd39bafd3873c0b1e
AlPerevyshin <alperevyshin@gmail.com>
2021-05-30 14:53:42 +0300

    Email tests

--- email/email.go
+++ email/email.go
@@ -2,18 +2,16 @@ package email
 
 import (
 	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"github.com/tusa-plus/core/utils"
-	"gopkg.in/gomail.v2"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/tusa-plus/core/utils"
+	"gopkg.in/gomail.v2"
 )
 
-const (
-	codeLen = 6
-)
-
+var ErrInvalidCode = fmt.Errorf("invalid code")
 var ConfigSmtpSender = os.Getenv("EMAIL_SENDER")
 var ConfigSmtpLogin = os.Getenv("EMAIL_USER")
 var ConfigSmtpPass = os.Getenv("EMAIL_PASSWORD")
@@ -32,39 +30,54 @@ type EmailVerification struct {
 	sender         CodeSender
 }
 
-func (ev *EmailVerification) SendCode(to string) error {
+func (ev *EmailVerification) SendCode(to string, symbols string, codeLen int) error {
+	ev.rndgen = utils.NewRandomGenerator(symbols)
 	code := ev.rndgen.NextString(codeLen)
-	ev.storage.Set(to, []byte(code), ev.codeExpiration)
-	err := ev.sender.SendCode(to, code)
+	err := ev.storage.Set(to, []byte(code), ev.codeExpiration)
+	if err != nil {
+		return err
+	}
+	err = ev.sender.SendCode(to, code)
 	return err
 }
 
 func (ev *EmailVerification) VerifyCode(code string, email string) error {
-	data, err := ev.storage.Get(code)
-	if err != nil || string(data) != email {
-		return fmt.Errorf("bad code")
+	data, err := ev.storage.Get(email)
+	if err != nil {
+		return err
+	}
+	if string(data) != code {
+		return ErrInvalidCode
 	}
 	return nil
 }
 
 type EmailCodeSender struct{}
 
-func (ecs *EmailCodeSender) SendCode(to string, code string) error {
+func (ecs *EmailCodeSender) EmailSend(sender, host, port, login, password, to, code string) error {
 	m := gomail.NewMessage()
 
 	m.SetBody("text/html", code)
 
 	m.SetHeaders(map[string][]string{
-		"From": {m.FormatAddress(ConfigSmtpSender, ConfigSmtpPort)},
+		"From": {m.FormatAddress(sender, port)},
 		"To":   {to},
 	})
 
-	port, err := strconv.Atoi(ConfigSmtpPort)
+	portInt, err := strconv.Atoi(port)
 	if err != nil {
 		return err
 	}
-	d := gomail.NewPlainDialer(ConfigSmtpHost, port, ConfigSmtpLogin, ConfigSmtpPass)
+	d := gomail.NewDialer(host, portInt, login, password)
 	err = d.DialAndSend(m)
+	return err
+}
+
+func (ecs *EmailCodeSender) SendCode(to string, code string) error {
+	err := ecs.EmailSend(
+		ConfigSmtpSender, ConfigSmtpHost, ConfigSmtpPort,
+		ConfigSmtpLogin, ConfigSmtpPass, to, code,
+	)
 	return err
 }
 
--- email/email_test.go
+++ email/email_test.go
@@ -1,31 +1,86 @@
 package email
 
 import (
-	"github.com/gofiber/storage/memory"
-	"github.com/tusa-plus/core/utils"
 	"testing"
 	"time"
+
+	"github.com/gofiber/storage/memory"
+	"github.com/tusa-plus/core/utils"
 )
 
 const (
 	validSymbols = "0123456789"
+	codeLen      = 6
+	smallTime    = time.Millisecond * 1000
+	bigTime      = time.Millisecond * 2200
 )
 
 var sender = &ChannelCodeSender{
-	channel: make(chan string),
+	channel: make(chan string, codeLen),
 }
 
 var ev = EmailVerification{
 	symbols:        validSymbols,
 	storage:        memory.New(),
-	codeExpiration: time.Second,
+	codeExpiration: time.Second * 2,
 	rndgen:         utils.NewRandomGenerator(validSymbols),
 	sender:         sender,
 }
 
 func Test_Generate(t *testing.T) {
-	err := ev.SendCode(ConfigSmtpSender)
+	err := ev.SendCode(ConfigSmtpSender, validSymbols, codeLen)
+	code := <-(sender.channel)
+	if err != nil || len(code) != codeLen {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(code) != codeLen {
+		t.Fatalf("invalid code len")
+	}
+}
+
+func Test_Verify(t *testing.T) {
+	err := ev.SendCode(ConfigSmtpSender, validSymbols, codeLen)
+	code := <-(sender.channel)
+	if err != nil || len(code) != codeLen {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(code) != codeLen {
+		t.Fatalf("invalid code len")
+	}
+	err = ev.VerifyCode(code, ConfigSmtpSender)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func Test_VerifySmallTimeout(t *testing.T) {
+	err := ev.SendCode(ConfigSmtpSender, validSymbols, codeLen)
+	code := <-(sender.channel)
+	if err != nil || len(code) != codeLen {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(code) != codeLen {
+		t.Fatalf("invalid code len")
+	}
+	time.Sleep(smallTime)
+	err = ev.VerifyCode(code, ConfigSmtpSender)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func Test_VerifyBigTimeout(t *testing.T) {
+	err := ev.SendCode(ConfigSmtpSender, validSymbols, codeLen)
+	code := <-(sender.channel)
+	if err != nil || len(code) != codeLen {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(code) != codeLen {
+		t.Fatalf("invalid code len")
+	}
+	time.Sleep(bigTime)
+	err = ev.VerifyCode(code, ConfigSmtpSender)
+	if err == nil {
+		t.Fatalf("Code wasn't delete")
 	}
 }
