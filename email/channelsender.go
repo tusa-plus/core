1165d0b68a02fe522544aaca0c805a347555775b
AlPerevyshin <alperevyshin@gmail.com>
2021-06-10 22:52:40 +0300

    Add default mock email verification

--- email/channelsender.go
+++ email/channelsender.go
@@ -1,10 +1,38 @@
 package email
 
+import (
+	"time"
+
+	"github.com/gofiber/storage/memory"
+	"github.com/tusa-plus/core/utils"
+)
+
 type ChannelCodeSender struct {
-	channel chan string
+	Channel chan string
 }
 
 func (ccs *ChannelCodeSender) SendCode(to string, code string) error {
-	ccs.channel <- code
+	ccs.Channel <- code
 	return nil
+}
+
+const (
+	validSymbols = "0123456789"
+	codeLen      = 6
+)
+
+func NewMockEmailVerification() (string, EmailVerification, *ChannelCodeSender) {
+	sender := &ChannelCodeSender{
+		Channel: make(chan string, codeLen),
+	}
+	ev := EmailVerification{
+		symbols:        validSymbols,
+		codeLen:        codeLen,
+		storage:        memory.New(),
+		codeExpiration: time.Second * 2,
+		rndgen:         utils.NewRandomGenerator(validSymbols),
+		sender:         sender,
+	}
+	userEmail := "xxxxxxx@gmail.com"
+	return userEmail, ev, sender
 }
