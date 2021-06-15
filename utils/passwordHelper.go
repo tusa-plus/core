7cd52c5eb1b804222431ef6f6706b96ce28b61b7
AlPerevyshin <alperevyshin@gmail.com>
2021-06-15 20:40:53 +0300

    Minor fix

--- utils/passwordHelper.go
+++ utils/passwordHelper.go
@@ -6,7 +6,7 @@ type PasswordHelper struct {
 	salt string
 }
 
-func (ph *PasswordHelper) HashPassword(password string, email string) [32]byte {
-	hash := sha256.Sum256([]byte(ph.salt + password + email))
+func (ph *PasswordHelper) HashPassword(password string, user string) [32]byte {
+	hash := sha256.Sum256([]byte(ph.salt + password + user))
 	return hash
 }
